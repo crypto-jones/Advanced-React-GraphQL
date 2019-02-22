b5c3ceb111c4b78784fdccb1aa293d33cc696c12
crypto-jones <integral.being@gmail.com>
2019-02-22 15:49:16 -0800

    Make basic home & sell pages

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Sell = props => {
+  return (
+    <div>
+      <p>Hey!</p>
+      <Link href="/">
+        <a>Home!</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Sell;
